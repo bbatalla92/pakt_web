3b243f92e3be50710801b8c2e9d3731f0473f72e
Brennan <brennan.batalla@gmail.com>
2016-12-11 16:49:23 -0500

    First Commit

---
+++ client/app/app.js
@@ -0,0 +1,29 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name paktApp
+ * @description
+ * # paktApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('app', [
+    'ngAnimate',
+    'ngAria',
+    'ngCookies',
+    'ngMessages',
+    'ngResource',
+    "ui.router",
+    'ngSanitize',
+    'ngTouch',
+    'ngMaterial',
+    'ngMdIcons'
+  ])
+  .config(function ( $urlRouterProvider, $locationProvider) {
+
+    $locationProvider.html5Mode(true);
+    $urlRouterProvider.otherwise('/');
+
+  });
