e10b448d0b1c7fafe1640e0449911064f5437b26
bbatall92 <brennan.batalla@gmail.com>
2017-03-07 20:25:17 -0500

    'commiting'

---
+++ client/components/toolbar/toolbar.js
@@ -0,0 +1,31 @@
+'use strict';
+
+(function(){
+
+
+angular.module('app')
+  .component("main",{
+    templateUrl: 'app/main/main.html',
+    controller: mainCtrl
+  })
+  .config(['$stateProvider',function ($stateProvider) {
+    $stateProvider
+      .state('main', {
+        url: '/',
+        template: '<main></main>'
+      });
+  }]);
+
+
+
+  function mainCtrl(){
+    var ctrl = this;
+
+
+
+
+
+  }
+
+
+})();
