71bcdabeaac079cbb9a639a32e6b975f465f78fa
bbatal200 <brennan_batalla@comcast.com>
2017-03-10 02:19:28 -0500

    Added search bar look and started the carousel look

---
+++ client/components/itemCarouselTemplate/itemCarouselTemplate.js
@@ -0,0 +1,23 @@
+'use strict';
+
+(function(){
+
+
+angular.module('app')
+  .component("itemCarouselTemplate",{
+    templateUrl: 'components/itemCarouselTemplate/itemCarouselTemplate.html',
+    controller: itemCarouselTemplateCtrl
+  });
+
+
+  function itemCarouselTemplateCtrl(){
+    var ctrl = this;
+
+
+
+
+
+  }
+
+
+})();
