ea39672b7ab31506cd28f4394f8fd315961b6e3c
bbatall92 <brennan.batalla@gmail.com>
2017-03-28 18:01:34 -0400

    Made some progress on items page

--- client/app/itemPage/itemPage.js
+++ client/app/itemPage/itemPage.js
@@ -1,26 +1,30 @@
-
-
-(function(){
+(function () {
   'use strict';
+  angular.module('app')
+    .component("itemPage", {
+      templateUrl: 'app/itemPage/itemPage.html',
+      controller: itemPageCtrl
+    })
+    .config(['$stateProvider', function ($stateProvider) {
+      $stateProvider
+        .state('item', {
+          url: '/item/:id',
+          template: '<item-page></item-page>',
+          params:{
+            item: null
+          }
+        });
+    }]);
 
+    itemPageCtrl.$inject = ["$stateParams"];
 
-angular.module('app')
-  .component("itemPage",{
-    templateUrl: 'app/itemPage/itemPage.html',
-    controller: itemPageCtrl
-  })
-  .config(['$stateProvider',function ($stateProvider) {
-    $stateProvider
-      .state('item', {
-        url: '/item/:id',
-        template: '<item-page></item-page>'
-      });
-  }]);
-
-
-
-  function itemPageCtrl(){
+  function itemPageCtrl($stateParams) {
     var ctrl = this;
+    ctrl.item = $stateParams.item;
+
+
+
+
 
 
   }
