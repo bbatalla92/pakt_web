36ee474bacf79da4ae30fabbf2c07b52d52e50b3
bbatal200 <brennan_batalla@comcast.com>
2017-04-23 19:19:42 -0400

    Item Page is now mobile ready

--- client/components/itemCarouselTemplate/itemCarouselTemplate.js
+++ client/components/itemCarouselTemplate/itemCarouselTemplate.js
@@ -1,24 +1,32 @@
 'use strict';
 
-(function(){
+(function () {
 
 
-angular.module('app')
-  .component("itemCarouselTemplate",{
-    templateUrl: 'components/itemCarouselTemplate/itemCarouselTemplate.html',
-    controller: itemCarouselTemplateCtrl,
-    bindings:{
-      item: "<"
-    }
-  });
+  angular.module('app')
+    .component("itemCarouselTemplate", {
+      templateUrl: 'components/itemCarouselTemplate/itemCarouselTemplate.html',
+      controller: itemCarouselTemplateCtrl,
+      bindings: {
+        item: "<"
+      }
+    });
 
+  itemCarouselTemplateCtrl.$inject = ["$timeout"];
 
-  function itemCarouselTemplateCtrl(){
+  function itemCarouselTemplateCtrl($timeout) {
     var ctrl = this;
 
 
+    function bootstrap() {
+      ctrl.image = {backgroundImage: "url(" + ctrl.item.imageUrls[0] + ")"};
+    }
 
 
+    // end of controller
+    $timeout(function () {
+      bootstrap()
+    }, 1)
 
   }
 
